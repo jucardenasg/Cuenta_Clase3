04ad69d49f214b0806a3a2014e4afd11d409ff75
Estudiante <Estudiante@10.203.1.110>
2018-08-22 09:03:02 -0500

    n

---
+++ JavaApplication4/src/javaapplication4/Cuenta.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication4;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Cuenta {
+    
+    //atributos
+    
+    public int number;
+    public String owner;
+    public float balance;
+    
+    public void setOwner(String _owner){
+    }
+    
+    public String getOwner(){
+    }
+    
+    public float setOwner(String _owner){
+    }
+    
+}
